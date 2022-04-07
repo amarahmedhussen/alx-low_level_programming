5f49af2a0784d949c30c1b8632ea292d85a82195
Amar <amarahmedhussen@gmail.com>
2022-04-07 09:56:47 -0700

    dkl

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,7 @@
 #include <unistd.h>                                                               
                                                                                   
 /**                                                                               
- * main - Entry point                                                             
- *                                                                                
+ * main - Entry point                                                          
  * Return: Always 0 (Success)                                                     
  */                                                                               
 int main(void)                                                                    
